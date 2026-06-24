b0c2d683419c258620f6062bf03b6d75009af094
Gamma-86 <bbcider445@gmail.com>
2026-06-24 23:07:28 +0500

    Added RAW return bitfield 32 type to IS returns

--- SourceCode/main/Kernel/OS_includes/OS_return_codes.ads
+++ SourceCode/main/Kernel/OS_includes/OS_return_codes.ads
@@ -17,6 +17,7 @@
 
 --IF THERE IS NO ERROR, RETURN ABSOLUTE 0
 
+
 package ReturnBitfields is
 
    type LOW8_ReturnSpace is 
@@ -32,6 +33,66 @@ package ReturnBitfields is
       GeneralOS_ReturnSpace_Extended1 => 3
    );
    for LOW8_ReturnSpace'Size use 8;
+
+type Return_Bitfield32_raw is record
+   LOW8_returnValNameSpace : LOW8_ReturnSpace;
+   Reserved8 : Boolean;
+   Reserved9 : Boolean;
+   Reserved10 : Boolean;
+   Reserved11 : Boolean;
+   Reserved12 : Boolean;
+   Reserved13 : Boolean;
+   Reserved14 : Boolean;
+   Reserved15 : Boolean;
+   Reserved16 : Boolean;
+   Reserved17 : Boolean;
+   Reserved18 : Boolean;
+   Reserved19 : Boolean;
+   Reserved20 : Boolean;
+   Reserved21 : Boolean;
+   Reserved22 : Boolean;
+   Reserved23 : Boolean;
+   Reserved24 : Boolean;
+   Reserved25 : Boolean;
+   Reserved26 : Boolean;
+   Reserved27 : Boolean;
+   Reserved28 : Boolean;
+   Reserved29 : Boolean;
+   Reserved30 : Boolean;
+   FulfilledWithError : Boolean;
+end record;
+for Return_Bitfield32_raw use record
+   LOW8_returnValNameSpace at 0 range 0 .. 7;
+
+   Reserved8 at 0 range 8 .. 8;
+   Reserved9 at 0 range 9 .. 9;
+   Reserved10 at 0 range 10 .. 10;
+   Reserved11 at 0 range 11 .. 11;
+   Reserved12 at 0 range 12 .. 12;
+   Reserved13 at 0 range 13 .. 13;
+   Reserved14 at 0 range 14 .. 14;
+   Reserved15 at 0 range 15 .. 15;
+
+   Reserved16 at 0 range 16 .. 16;
+   Reserved17 at 0 range 17 .. 17;
+   Reserved18 at 0 range 18 .. 18;
+   Reserved19 at 0 range 19 .. 19;
+   Reserved20 at 0 range 20 .. 20;
+   Reserved21 at 0 range 21 .. 21;
+   Reserved22 at 0 range 22 .. 22;
+   Reserved23 at 0 range 23 .. 23;
+
+   Reserved24 at 0 range 24 .. 24;
+   Reserved25 at 0 range 25 .. 25;
+   Reserved26 at 0 range 26 .. 26;
+   Reserved27 at 0 range 27 .. 27;
+   Reserved28 at 0 range 28 .. 28;
+   Reserved29 at 0 range 29 .. 29;
+   Reserved30 at 0 range 30 .. 30;
+
+   FulfilledWithError at 0 range 31 .. 31;
+end record;
+for Return_Bitfield32_raw'Size use 32;
 
 type GeneralOS is record
    ReturnValNameSpace : LOW8_ReturnSpace;
@@ -75,58 +136,22 @@ for GeneralOS use record
    TooBigNumber         at 0 range 16 .. 16;
    TooSmallNumber       at 0 range 17 .. 17;
 
-   Reserved18 at 2 range 2 .. 2;
-   Reserved19 at 2 range 3 .. 3;
-   Reserved20 at 2 range 4 .. 4;
-   Reserved21 at 2 range 5 .. 5;
-   Reserved22 at 2 range 6 .. 6;
-   Reserved23 at 2 range 7 .. 7;
+   Reserved18 at 0 range 18 .. 18;
+   Reserved19 at 0 range 19 .. 19;
+   Reserved20 at 0 range 20 .. 20;
+   Reserved21 at 0 range 21 .. 21;
+   Reserved22 at 0 range 22 .. 22;
+   Reserved23 at 0 range 23..  23;
       
-   Reserved24 at 3 range 0 .. 0;
-   Reserved25 at 3 range 1 .. 1;
-   Reserved26 at 3 range 2 .. 2;
-   Reserved27 at 3 range 3 .. 3;
-   Reserved28 at 3 range 4 .. 4;
-   Reserved29 at 3 range 5 .. 5;
-   Reserved30 at 3 range 6 .. 6;
-   Reserved31 at 3 range 7 .. 7;
+   Reserved24 at 0 range 24 .. 24;
+   Reserved25 at 0 range 25 .. 25;
+   Reserved26 at 0 range 26 .. 26;
+   Reserved27 at 0 range 27 .. 27;
+   Reserved28 at 0 range 28 .. 28;
+   Reserved29 at 0 range 29 .. 29;
+   Reserved30 at 0 range 30 .. 30;
+   FulfilledWithError at 0 range 31 .. 31;
 end record;
-
-   type GeneralOSReturnCodes_enum is 
-   (
-   SuccessCode,
-   LOW8_GeneralOSErrorSpace,
-   LOW8_GeneralOSErrorSpace_Extended1,
-      HIGH_OtherErrorBit,
-      HIGH_NULLptrErrorBit,
-      HIGH_InsertOverflowBit,
-      HIGH_ArrayInsertUnableBit,
-      HIGH_ArrayIndexOverflowBit,
-      HIGH_AllocationFailedBit,
-      HIGH_AllocationImpossibleBit,
-      HIGH_SpinlockWatchdogSetBit,
-
-
-      HIGH_TaskFulfilWithErrorBit
-   );
-   for GeneralOSReturnCodes_enum use
-   (
-   SuccessCode = 0,
-   LOW8_GeneralOSErrorSpace => 2,
-   LOW8_GeneralOSErrorSpace_Extended1 => 3,
-      HIGH_OtherErrorBit =>16#0100#,
-      HIGH_NULLptrErrorBit =>16#0200#,
-      HIGH_InsertOverflowBit =>16#0400#,
-      HIGH_ArrayInsertUnableBit =>16#0800#,
-      HIGH_ArrayIndexOverflowBit => 16#1000#,
-      HIGH_AllocationFailedBit => 16#2000#,
-      HIGH_AllocationImpossibleBit => 16#4000#,
-      HIGH_SpinlockWatchdogSetBit => 16#8000#,
-
-   
-      HIGH_TaskFulfilWithErrorBit => 16#80000000#
-   );
-
-private
+for GeneralOS'Size use 32;
 
 end OS_return_codes;
